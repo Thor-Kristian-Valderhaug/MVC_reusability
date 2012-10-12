17d91c96c2aa9c5220281bd571e0cfca7f4a9975
Thor Kristian Valderhaug <tk@tksoftware.no>
2012-10-12 19:20:44 +0200

    Added the BaseController and BaseModel

---
+++ MVC_reusability/Controllers/BaseController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using MVC_reusability.Models;
+
+namespace MVC_reusability.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected T GetModel<T>(ActionResult result) where T : BaseModel, new ()
+        {
+            var viewResult = (ViewResultBase) result;
+            var model = viewResult.Model.To<T>();
+            return model;
+        }
+
+         protected ViewResult BaseView(object model)
+         {
+             var viewName = RouteData.Values["Action"] as string;
+             var controllerName = RouteData.Values["Controller"] as string;
+             var masterName = string.Format("~/View/{0}/{1}.cshtml", controllerName, viewName);
+
+             return View("_Empty", masterName, model);
+         }
+
+         protected ViewResult NestedView(object model)
+         {
+             var viewName = RouteData.Values["Action"] as string;
+             var controllerName = RouteData.Values["Controller"] as string;
+             var masterName = string.Format("~/View/{0}/{1}.cshtml", controllerName, viewName);
+
+             return View(viewName, masterName, model);
+         }
+    }
+}
---
+++ MVC_reusability/Models/BaseModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AutoMapper;
+
+namespace MVC_reusability.Models
+{
+    public class BaseModel
+    {
+     
+    
+    }
+
+
+
+    public static class ModelExtensions
+    {
+        public static T To<T> (this object baseModel ) where T :  BaseModel, new ()
+        {
+            if (baseModel == null || !(baseModel is BaseModel))
+                return Activator.CreateInstance<T>();
+
+            Mapper.CreateMap(baseModel.GetType(), typeof (T));
+            return Mapper.Map<T>(baseModel);
+
+        }
+    }
+}
