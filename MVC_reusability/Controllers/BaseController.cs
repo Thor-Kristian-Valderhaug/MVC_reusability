4aedecee17c1fa6f8cc03ec9140766b8889c22cc
Thor Kristian Valderhaug <thor.k.valderhaug@bekk.no>
2012-10-23 13:56:17 +0200

    removed unused function in baseController

--- MVC_reusability/Controllers/BaseController.cs
+++ MVC_reusability/Controllers/BaseController.cs
@@ -21,15 +21,6 @@ namespace MVC_reusability.Controllers
              return View("_Empty", masterName, model);
          }
 
-         protected ViewResult NestedView(object model = null)
-         {
-             var viewName = RouteData.Values["Action"] as string;
-             var controllerName = RouteData.Values["Controller"] as string;
-             var masterName = string.Format("~/Views/{0}/{1}.cshtml", controllerName, viewName);
-
-             return View(viewName, masterName, model);
-         }
-
         protected bool NotViewResult(ActionResult result)
         {
             return !(result is ViewResultBase);
